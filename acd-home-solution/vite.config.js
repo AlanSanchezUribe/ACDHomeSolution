b133ae5ac0487172f7ac9178a33f17f01a2e7818
Alan Sanchez <34654682+Kdthug@users.noreply.github.com>
2025-11-10 14:44:00 -0300

    docs

--- acd-home-solution/vite.config.js
+++ acd-home-solution/vite.config.js
@@ -5,5 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/ACDHomeSolution/",
+  build: { outDir: "../docs", emptyOutDir: true },
   plugins: [react(), tailwindcss()],
 });
