96380e96f127f31dde4afce68f7cc1f6a3a38598
Alan Sanchez <34654682+Kdthug@users.noreply.github.com>
2025-11-10 14:13:33 -0300

    publicar

--- acd-home-solution/vite.config.js
+++ acd-home-solution/vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/ACDHomeSolution/",
   plugins: [react(), tailwindcss()],
-})
+});
